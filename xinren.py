2c6611548413298e8433991c9fb10e26ff7a48b8
Jie <wangjie19940416@icloud.com>
2018-03-06 15:34:17 +0800

    read the data

--- xinren.py
+++ xinren.py
@@ -5,6 +5,14 @@ Created on Tue Mar  6 15:03:11 2018
 @author: wangj
 """
 
-print('helloworld')
+import numpy as np
+import pandas as pd
 
-print('helloworld')
+#读取数据集
+path = 'C:/Users/wangj/Documents/tianchi/xinren/fresh_comp_offline'
+
+tianchi_fresh_comp_train_item = pd.read_csv(path + '/tianchi_fresh_comp_train_item.csv')
+tianchi_fresh_comp_train_user = pd.read_csv(path + '/tianchi_fresh_comp_train_user.csv')
+
+tianchi_fresh_comp_train_item.head(5)
+tianchi_fresh_comp_train_user.head(5)
